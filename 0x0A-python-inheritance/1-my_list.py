f55a72fca9e249dccb01cf6728c51ffb189d736f
Zeekdrich <abahisaac16@gmail.com>
2023-01-10 11:55:37 -0800

    commit two

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,4 +6,4 @@ class MyList(list):
     """The class that inherits from list"""
     def print_sorted(self):
         """Prints the list in ascending sort"""
-        print(sorted(self))`
+        print(sorted(self))
