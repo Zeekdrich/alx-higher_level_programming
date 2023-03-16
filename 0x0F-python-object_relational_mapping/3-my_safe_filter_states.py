386762a0f599cca9f24d8ee5f42e6412a75361ac
Zeekdrich <abahisaac16@gmail.com>
2023-03-16 05:06:58 -0700

    all task

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N (upper N) from
-   the database hbtn_0e_0_usa"""
+"""Takes in arguments and displays all values in the states table of
+   hbtn_0e_0_usa where name matches the argument, safe from MySQL injections!
+"""
 
-import sys
 import MySQLdb
+import sys
 
 if __name__ == '__main__':
+
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id ASC;""")
+    cur.execute("SELECT * FROM states WHERE name =%s\
+                  ORDER BY states.id ASC", (sys.argv[4],))
     rows = cur.fetchall()
     for r in rows:
         print(r)
